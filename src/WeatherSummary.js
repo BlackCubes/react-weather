417444ee124b49cd5e7f286dd1113bc7c0f27b2f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-01-17 10:59:56 -0800

    Added temp conversion with character code method

--- src/WeatherSummary.js
+++ src/WeatherSummary.js
@@ -13,60 +13,67 @@ const WeatherDetails = ({
   precipitation,
   humidity,
   windSpeed,
-}) => (
-  <div className="weather-details">
-    <div className="weather-details__header">
-      <div className="weather-details__header-location">{location}</div>
+}) => {
+  const tempConversion = (temp) =>
+    units === 'metric' ? (temp - 32) / 1.8 : temp;
 
-      <div className="weather-details__header-day">{dayOfWeek}</div>
+  return (
+    <div className="weather-details">
+      <div className="weather-details__header">
+        <div className="weather-details__header-location">{location}</div>
 
-      <div className="weather-details__header-weather">{weatherCondition}</div>
-    </div>
-    <div className="weather-details__condition">
-      <div className="weather-details__condition--primary">
-        <img
-          className="weather-details__condition-icon"
-          src={`${icon}`}
-          alt="An icon"
-        />
+        <div className="weather-details__header-day">{dayOfWeek}</div>
 
-        <div className="weather-details__condition-temp">
-          <span className="weather-details__condition-temp-value">
-            {currentTemp}
-          </span>
+        <div className="weather-details__header-weather">
+          {weatherCondition}
+        </div>
+      </div>
+      <div className="weather-details__condition">
+        <div className="weather-details__condition--primary">
+          <img
+            className="weather-details__condition-icon"
+            src={`${icon}`}
+            alt="An icon"
+          />
 
-          <div className="weather-details__condition-temp-unit">
-            <span>&deg; F</span>
-            &nbsp; | &nbsp;
-            <span>&deg; C</span>
+          <div className="weather-details__condition-temp">
+            <span className="weather-details__condition-temp-value">
+              {tempConversion(currentTemp)}
+            </span>
+
+            <div className="weather-details__condition-temp-unit">
+              <span>&deg; F</span>
+              &nbsp; | &nbsp;
+              <span>&deg; C</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="weather-details__condition--secondary">
+          <div className="weather-details__condition-hightemp">
+            {`High: ${tempConversion(highTemp) + String.fromCharCode(176)}`}
+          </div>
+
+          <div className="weather-details__condition-lowtemp">
+            {`Low: ${tempConversion(lowTemp) + String.fromCharCode(176)}`}
+          </div>
+
+          <div className="weather-details__condition-precipitation">
+            {`Precipitation: ${precipitation}`}
+          </div>
+
+          <div className="weather-details__condition-humidity">
+            {`Humidity: ${humidity}`}
+          </div>
+
+          <div className="weather-details__condition-windspeed">
+            {`Wind: ${windSpeed}`}
           </div>
         </div>
       </div>
-
-      <div className="weather-details__condition--secondary">
-        <div className="weather-details__condition-hightemp">
-          {`High: ${highTemp}`}
-        </div>
-
-        <div className="weather-details__condition-lowtemp">
-          {`Low: ${lowTemp}`}
-        </div>
-
-        <div className="weather-details__condition-precipitation">
-          {`Precipitation: ${precipitation}`}
-        </div>
-
-        <div className="weather-details__condition-humidity">
-          {`Humidity: ${humidity}`}
-        </div>
-
-        <div className="weather-details__condition-windspeed">
-          {`Wind: ${windSpeed}`}
-        </div>
-      </div>
     </div>
-  </div>
-);
+  );
+};
 
 WeatherDetails.propTypes = {
   location: PropTypes.string.isRequired,
