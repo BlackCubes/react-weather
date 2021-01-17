6728b43908f598e24e86d7fe3adcdfaa775a9383
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-01-17 15:15:39 -0800

    Added wrapper

--- src/WeatherSummary.js
+++ src/WeatherSummary.js
@@ -23,54 +23,56 @@ const WeatherDetails = ({
     units === 'metric' ? (temp - 32) / 1.8 : temp;
 
   return (
-    <div className="weather-summary">
-      <div className="weather-summary__header">
-        <div className="weather-summary__header-location">{location}</div>
+    <div className="weather-summary-wrapper">
+      <div className="weather-summary">
+        <div className="weather-summary__header">
+          <div className="weather-summary__header-location">{location}</div>
 
-        <div className="weather-summary__header-day">{dayOfWeek}</div>
+          <div className="weather-summary__header-day">{dayOfWeek}</div>
 
-        <div className="weather-summary__header-weather">
-          {weatherCondition}
-        </div>
-      </div>
-      <div className="weather-summary__condition">
-        <div className="weather-summary__condition--primary">
-          <div className="weather-summary__condition-icon">
-            <FontAwesomeIcon icon={icon} />
+          <div className="weather-summary__header-weather">
+            {weatherCondition}
           </div>
+        </div>
+        <div className="weather-summary__condition">
+          <div className="weather-summary__condition--primary">
+            <div className="weather-summary__condition-icon">
+              <FontAwesomeIcon icon={icon} />
+            </div>
 
-          <div className="weather-summary__condition-temp">
-            <span className="weather-summary__condition-temp-value">
-              {tempConversion(currentTemp)}
-            </span>
+            <div className="weather-summary__condition-temp">
+              <span className="weather-summary__condition-temp-value">
+                {tempConversion(currentTemp)}
+              </span>
 
-            <div className="weather-summary__condition-temp-unit">
-              <span>&deg; F</span>
-              &nbsp; | &nbsp;
-              <span>&deg; C</span>
+              <div className="weather-summary__condition-temp-unit">
+                <span>&deg; F</span>
+                &nbsp; | &nbsp;
+                <span>&deg; C</span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="weather-summary__condition--secondary">
-          <div className="weather-summary__condition-hightemp">
-            {`High: ${tempConversion(highTemp) + String.fromCharCode(176)}`}
-          </div>
+          <div className="weather-summary__condition--secondary">
+            <div className="weather-summary__condition-hightemp">
+              {`High: ${tempConversion(highTemp) + String.fromCharCode(176)}`}
+            </div>
 
-          <div className="weather-summary__condition-lowtemp">
-            {`Low: ${tempConversion(lowTemp) + String.fromCharCode(176)}`}
-          </div>
+            <div className="weather-summary__condition-lowtemp">
+              {`Low: ${tempConversion(lowTemp) + String.fromCharCode(176)}`}
+            </div>
 
-          <div className="weather-summary__condition-precipitation">
-            {`Precipitation: ${precipitation + String.fromCharCode(37)}`}
-          </div>
+            <div className="weather-summary__condition-precipitation">
+              {`Precipitation: ${precipitation + String.fromCharCode(37)}`}
+            </div>
 
-          <div className="weather-summary__condition-humidity">
-            {`Humidity: ${humidity + String.fromCharCode(37)}`}
-          </div>
+            <div className="weather-summary__condition-humidity">
+              {`Humidity: ${humidity + String.fromCharCode(37)}`}
+            </div>
 
-          <div className="weather-summary__condition-windspeed">
-            {`Wind: ${windSpeed} mph`}
+            <div className="weather-summary__condition-windspeed">
+              {`Wind: ${windSpeed} mph`}
+            </div>
           </div>
         </div>
       </div>
