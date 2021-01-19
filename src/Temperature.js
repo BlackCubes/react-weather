9c16bd504a9b48428bfdf622dbf15ef803803b64
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-01-18 18:30:06 -0800

    Expanded with unit conversion and another required prop

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
 
-const Temperature = ({ temp }) => temp;
+const Temperature = ({ temp, units }) =>
+  units === 'metric' ? (temp - 32) / 1.8 : temp;
 
 Temperature.propTypes = {
   temp: PropTypes.number.isRequired,
+  units: PropTypes.oneOf(['imperial', 'metric']).isRequired,
 };
 
 export default Temperature;
