f35284ce9570d267c9ceab73b96134a97c7f3d34
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-01-19 19:58:55 -0800

    Created a class component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 import { getCoordinates, getWeatherData } from './util';
 
-function App() {
-  const [lat, setLat] = React.useState(false);
-  const [lon, setLon] = React.useState(false);
-  const [weather, setWeather] = React.useState({});
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      weather: '',
+      error: '',
+    };
+  }
 
-  React.useEffect(() => {
-    getCoordinates('Fresno').then((data) => {
-      setLat(data.lat);
-      setLon(data.lng);
-    });
-  }, []);
-
-  React.useEffect(() => {
-    if (lat && lon) getWeatherData(lat, lon).then((data) => setWeather(data));
-  }, [lat, lon]);
-
-  console.log('lat: ', lat);
-  console.log('lon: ', lon);
-  console.log('weather: ', weather);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    const successGeo = (pos) => {
+      console.log(pos);
+      this.setState({ weather: 'something' });
+      this.setState({ isLoading: false });
+      this.setState({ error: 'none' });
+    };
+    const errorGeo = (err) => console.log(err);
+    navigator.geolocation.getCurrentPosition(successGeo, errorGeo);
+  }
 }
+
+// function App() {
+//   const [lat, setLat] = React.useState(false);
+//   const [lon, setLon] = React.useState(false);
+//   const [weather, setWeather] = React.useState({});
+
+//   React.useEffect(() => {
+//     getCoordinates('Fresno').then((data) => {
+//       setLat(data.lat);
+//       setLon(data.lng);
+//     });
+//   }, []);
+
+//   React.useEffect(() => {
+//     if (lat && lon) getWeatherData(lat, lon).then((data) => setWeather(data));
+//   }, [lat, lon]);
+
+//   console.log('lat: ', lat);
+//   console.log('lon: ', lon);
+//   console.log('weather: ', weather);
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         {/* <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p> */}
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
