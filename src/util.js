467af10a8d6b4141af2835f5abc12adf7c317d7e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-01-22 18:34:25 -0800

    Removed the current data object

--- src/util.js
+++ src/util.js
@@ -14,7 +14,7 @@ export const getWeatherData = async (lat, lon) => {
   try {
     const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPENWEATHER_API}`;
     const res = await axios.get(apiUrl);
-    if (res.status === 200) return res.data.current;
+    if (res.status === 200) return res.data;
   } catch (err) {
     console.log(err);
   }
