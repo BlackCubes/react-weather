5892aa1d4b7fcd6bfaffb5a4ec11941fd20b2332
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-01-23 14:23:04 -0800

    Created a weather icon component

---
+++ src/WeatherIcon.js
@@ -0,0 +1,14 @@
+import { icon } from '@fortawesome/fontawesome-svg-core';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const WeatherIcon = ({ weatherIcon }) => (
+  <img
+    src={`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`}
+    alt="Weather icon"
+  />
+);
+
+WeatherIcon.propTypes = { weatherIcon: PropTypes.string.isRequired };
+
+export default WeatherIcon;
