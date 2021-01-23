fc80f54ce8167141874e78b6408fbb8ab62e7b51
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-01-23 10:03:44 -0800

    Commented out the wrapper

--- src/WeatherSummary.js
+++ src/WeatherSummary.js
@@ -2,19 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const WeatherSummary = ({ dayOfWeek, icon, highTemp, lowTemp }) => (
-  <div className="weather-summary-wrapper">
-    <div className="weather-summary">
-      <div className="weather-summary__day">{dayOfWeek}</div>
+  // <div className="weather-summary-wrapper">
+  <div className="weather-summary">
+    <div className="weather-summary__day">{dayOfWeek}</div>
 
-      <div className="weather-summary__icon">{icon}</div>
+    <div className="weather-summary__icon">{icon}</div>
 
-      <div className="weather-summary__temp">
-        <div className="weather-summary__temp-high">{highTemp}</div>
+    <div className="weather-summary__temp">
+      <div className="weather-summary__temp-high">{highTemp}</div>
 
-        <div className="weather-summary__temp-low">{lowTemp}</div>
-      </div>
+      <div className="weather-summary__temp-low">{lowTemp}</div>
     </div>
   </div>
+  // </div>
 );
 
 WeatherSummary.propTypes = {
