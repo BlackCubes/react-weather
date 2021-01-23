783e76ca3c6a731e2f8f3e026233c8e6a7898301
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-01-22 20:21:22 -0800

    Created a get location function with mapquest api

--- src/util.js
+++ src/util.js
@@ -11,6 +11,16 @@ export const getCoordinates = async (location) => {
   }
 };
 
+export const getLocation = async (lat, lon) => {
+  try {
+    const apiUrl = `http://www.mapquestapi.com/geocoding/v1/reverse?${process.env.REACT_APP_MAPQUEST_API}&location=${lat},${lon}`;
+    const res = await axios.get(apiUrl);
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const getWeatherData = async (lat, lon) => {
   try {
     const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPENWEATHER_API}&units=imperial`;
