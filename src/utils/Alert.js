57660746debcaa7fb6d62162f2c828ee8c210d9d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-01-30 12:16:43 -0800

    Refactored the function into react

--- src/utils/Alert.js
+++ src/utils/Alert.js
@@ -1,11 +1,33 @@
-export const hideAlert = () => {
-  const el = document.querySelector('.alert');
-  if (el) el.parentElement.removeChild(el);
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// export const hideAlert = () => {
+//   const el = document.querySelector('.alert');
+//   if (el) el.parentElement.removeChild(el);
+// };
+
+// export const showAlert = (type, msg) => {
+//   hideAlert();
+//   const markup = `<div class="alert alert--${type}">${msg}</div>`;
+//   document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+//   window.setTimeout(hideAlert, 10000);
+// };
+
+const Alert = ({ message, type }) => {
+  let styleDisplay = 'block';
+  setTimeout(() => {
+    styleDisplay = 'none';
+  }, 5000);
+  return (
+    <div className={`alert alert--${type}`} style={{ display: styleDisplay }}>
+      {message}
+    </div>
+  );
 };
 
-export const showAlert = (type, msg) => {
-  hideAlert();
-  const markup = `<div class="alert alert--${type}">${msg}</div>`;
-  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
-  window.setTimeout(hideAlert, 10000);
+Alert.propTypes = {
+  message: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 };
+
+export default Alert;
