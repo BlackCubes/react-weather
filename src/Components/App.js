c3085878d047009d0d1f69e06a0bebd39f28ed25
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-01-30 12:31:40 -0800

    Refactored to display the error message with the main content

--- src/Components/App.js
+++ src/Components/App.js
@@ -122,73 +122,69 @@ class App extends React.Component {
   render() {
     const { isLoading, error, weather, location, index, tempUnit } = this.state;
 
-    const renderedContent = error ? (
-      // <div className="paragraph error">{error}</div>
-      <Alert message={error} type="error" />
-    ) : (
-      weather && (
-        // <div className="weather-wrapper">
-        <>
-          <SearchBar onSubmit={this.onSubmit} />
-          <WeatherDetails
-            location={location}
-            dayOfWeek={dateTimeFormat(
-              'en-US',
-              { weekday: 'long' },
-              unixToDateTime(
-                index === 0 ? weather.current.dt : weather.daily[index].dt
-              )
-            )}
-            weatherCondition={
-              index === 0
-                ? weather.current.weather[0].description
-                : weather.daily[index].weather[0].description
-            }
-            icon={
-              index === 0
-                ? weather.current.weather[0].icon
-                : weather.daily[index].weather[0].icon
-            }
-            currentTemp={Math.round(
-              index === 0 ? weather.current.temp : weather.daily[index].temp.day
-            )}
-            highTemp={Math.round(
-              index === 0
-                ? weather.daily[0].temp.max
-                : weather.daily[index].temp.max
-            )}
-            lowTemp={Math.round(
-              index === 0
-                ? weather.daily[0].temp.min
-                : weather.daily[index].temp.min
-            )}
-            precipitation={
-              index === 0
-                ? Math.round(weather.daily[0].pop * 100)
-                : Math.round(weather.daily[index].pop * 100)
-            }
-            humidity={
-              index === 0
-                ? weather.current.humidity
-                : weather.daily[index].humidity
-            }
-            windSpeed={Math.round(
-              index === 0
-                ? weather.current.wind_speed
-                : weather.daily[index].wind_speed
-            )}
-            convertTempUnits={this.convertTempUnits}
-            units={tempUnit}
-          />
+    const renderedContent = weather && (
+      // <div className="weather-wrapper">
+      <>
+        {error && <Alert message={error} type="error" />}
+        <SearchBar onSubmit={this.onSubmit} />
+        <WeatherDetails
+          location={location}
+          dayOfWeek={dateTimeFormat(
+            'en-US',
+            { weekday: 'long' },
+            unixToDateTime(
+              index === 0 ? weather.current.dt : weather.daily[index].dt
+            )
+          )}
+          weatherCondition={
+            index === 0
+              ? weather.current.weather[0].description
+              : weather.daily[index].weather[0].description
+          }
+          icon={
+            index === 0
+              ? weather.current.weather[0].icon
+              : weather.daily[index].weather[0].icon
+          }
+          currentTemp={Math.round(
+            index === 0 ? weather.current.temp : weather.daily[index].temp.day
+          )}
+          highTemp={Math.round(
+            index === 0
+              ? weather.daily[0].temp.max
+              : weather.daily[index].temp.max
+          )}
+          lowTemp={Math.round(
+            index === 0
+              ? weather.daily[0].temp.min
+              : weather.daily[index].temp.min
+          )}
+          precipitation={
+            index === 0
+              ? Math.round(weather.daily[0].pop * 100)
+              : Math.round(weather.daily[index].pop * 100)
+          }
+          humidity={
+            index === 0
+              ? weather.current.humidity
+              : weather.daily[index].humidity
+          }
+          windSpeed={Math.round(
+            index === 0
+              ? weather.current.wind_speed
+              : weather.daily[index].wind_speed
+          )}
+          convertTempUnits={this.convertTempUnits}
+          units={tempUnit}
+        />
 
-          <WeatherForecast
-            forecast={weather.daily}
-            getIndexFromComp={this.getIndexFromComp}
-            activeClass={this.activeClass}
-            units={tempUnit}
-          />
-        </>
-      )
+        <WeatherForecast
+          forecast={weather.daily}
+          getIndexFromComp={this.getIndexFromComp}
+          activeClass={this.activeClass}
+          units={tempUnit}
+        />
+      </>
     );
 
     return (
